05579785492c72c1ab3a2e69035446b0a9a1a4ca
Justin Lin <justinlin@JustinTzHLins-MacBook-Pro.local>
2024-08-23 00:04:49 -0700

    Integrated Gemini 1.5 Flash into Brainsparl-Lab: You can ask Gemini about the explanation of the options now

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const cors = require("cors");
 const userRoute = require('./routes/userRoute');
 const quizRoute = require('./routes/quizRoute');
+const geminiRoute = require('./routes/geminiRoute');
 
 const PORT = 3001;
 const dev = process.env.NODE_ENV !== "production";
@@ -24,6 +25,9 @@ app.prepare().then(() => {
   server.use(express.urlencoded({ extended: true }));
 
   console.log(server.settings.env);
+
+  // gemini
+  server.use('/gemini', geminiRoute);
 
   // server side routing
   server.use('/user', userRoute);
